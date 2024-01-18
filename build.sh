007b66240b191a2bfdaec45f1d3fe9337a5f8b41
Steve Shipway <steve@steveshipway.org>
2024-01-18 14:45:03 +1300

    Build repository

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo Ensuring were using the correct branch
+git checkout gh-pages
+echo Merging from master
+git pull origin master
+
+echo Building packages
+helm package step-certificates
+helm package step-issuer
+helm package autocert
+
+echo Building index
+helm repo index `pwd` --url https://smx-ltd.github.io/smallstep-helm-charts/
+
+echo Sending up to github
+git add .
+git commit -m "Build repository"
+git push
+
+echo Repo site will now automatically deploy
+
+exit 0
